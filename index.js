04216dab19d2dec4ba7a4f3c883dd314c96740ad
Dilip Hiremath <dilip.s.hiremath@gmail.com>
2021-11-09 20:21:32 +0530

    add public front end

--- index.js
+++ index.js
@@ -10,10 +10,13 @@ const app = express()
 // Rate limiter
 const limiter = rateLimit({
     windowMs: 10 * 60 * 1000, // 10 Mins
-    max: 5
+    max: 30
 })
 app.use(limiter)
 app.set('trust proxy', 1)
+
+// Set static folder
+app.use(express.static('public'))
 
 // Routes
 app.use('/api', require('./routes'))
